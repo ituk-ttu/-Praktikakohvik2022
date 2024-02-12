9b96727e9b926ec2323dd178a1e05aa29115b97d
Holger Nõgu <honogu@ttu.ee>
2024-02-12 21:48:19 +0200

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/pk-app/sw.js', { scope: '/pk-app/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
